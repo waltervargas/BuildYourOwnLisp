a74bfa73ac72b32e651d656f793f036043768684
Walter V <waltervargas@linux.com>
2019-01-09 21:09:17 +0000

    Initial commit

---
+++ src/parsing.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpc.h"
+
+/* For compiling on windows */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/*Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#include <histedit.h>
+#endif
+
+int main(int argc, char** argv) {
+
+  /* Parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lispy    = mpc_new("lispy");
+
+  /* Define them with the following Language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                    \
+              number   : /-?[0-9]+/ ;                            \
+              operator : '+' | '-' | '*' | '/' ;                 \
+              expr     : <number> | '(' <operator> <expr>+ ')' ; \
+              lispy    : /^/ <operator> <expr>+ /$/ ;            \
+            ",
+            Number, Operator, Expr, Lispy);
+
+  /* Print Version and Exit Information */
+  puts("Lispy Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+
+  /* In a never ending loop */
+  while (1) {
+
+    /* Output our prompt */
+    char* input = readline("lambda> ");
+
+    /* Add input to history */
+    add_history(input);
+
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      /* On Success Print the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise Print the Error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
+    /* Free retrieved input */
+    free(input);
+  }
+
+  /* Undefine and Delete Parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  return 0;
+}
